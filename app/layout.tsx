117163a18f85ff716aafdb8db155097cb5e4498d
Jann Jaspher Santos <75710250+thisbejann@users.noreply.github.com>
2023-04-12 21:18:04 +0800

    finish login modal and function

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUsers";
 
 export const metadata = {
   title: "Airbnb Clone",
@@ -16,14 +17,17 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
